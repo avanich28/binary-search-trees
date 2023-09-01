a9f2d59e596f3b635d77447132ff223e884d6c9b
avanich28 <arpawan.v@gmail.com>
2023-09-01 15:07:39 +0700

    clean code

--- script.js
+++ script.js
@@ -258,6 +258,18 @@ const myTree = new Tree(
   mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 );
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) return;
+
+  if (node.right !== null)
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+
+  if (node.left !== null)
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
+
 myTree.insert(28);
 myTree.insert(29);
 myTree.insert(30);
@@ -272,16 +284,5 @@ console.log(myTree.depth(myTree.find(23))); // 2
 console.log(myTree.isBalanced()); // false
 const reBalanceTree = myTree.rebalance(myTree.inOrder());
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) return;
-
-  if (node.right !== null)
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-
-  if (node.left !== null)
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-};
 prettyPrint(myTree.root);
 prettyPrint(reBalanceTree);
